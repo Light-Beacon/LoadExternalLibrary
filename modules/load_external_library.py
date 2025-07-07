30b7d3abaa002d2d737eee68fba0fcf1fd7f1c8b
Nattiden <2682645990@qq.com>
2025-07-07 08:53:35 +0800

    use path join

--- modules/load_external_library.py
+++ modules/load_external_library.py
@@ -1,4 +1,4 @@
-from os import sep
+import os
 from homepagebuilder.interfaces.Events import on
 from homepagebuilder.core.io import File
 from homepagebuilder.core.project import Project
@@ -9,7 +9,6 @@ LIBRARIES_PATH = ['external_library']
 def load_external_libraries(project:Project, *args, **kwargs):
     library_files = []
     for library_path in LIBRARIES_PATH:
-        library_files.append(File(project.base_path + sep + library_path +
-                                sep + "__LIBRARY__.yml"))
+        library_files.append(File(os.path.join(project.base_path, library_path, "__LIBRARY__.yml")))
     project.base_library.add_sub_libraries(library_files)
     print(f"Loaded external libraries from: {', '.join(LIBRARIES_PATH)}")
